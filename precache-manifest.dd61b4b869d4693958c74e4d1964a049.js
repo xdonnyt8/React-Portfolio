74d0eb39b0b752e1dcd01b454c95ff8ffef22798
Don Tran <mr.dontran@gmail.com>
2020-10-21 02:26:51 -0400

    Updates

---
+++ precache-manifest.dd61b4b869d4693958c74e4d1964a049.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc503b3b2694bcaa964520bc34bb93d4",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "8ce0ef077771a8c6cdb1",
+    "url": "/React-Portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "d122f1ba96e26470ac83",
+    "url": "/React-Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ce0ef077771a8c6cdb1",
+    "url": "/React-Portfolio/static/js/2.01ee2e84.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/React-Portfolio/static/js/2.01ee2e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d122f1ba96e26470ac83",
+    "url": "/React-Portfolio/static/js/main.fe6fb3a2.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "c58c78c292d3b07c852c687b0e1a3267",
+    "url": "/React-Portfolio/static/media/DSC03937.c58c78c2.jpg"
+  },
+  {
+    "revision": "d3431592eeca86a90cbb9f7a73efbd6a",
+    "url": "/React-Portfolio/static/media/date.d3431592.png"
+  },
+  {
+    "revision": "3982276396c35728645f2ebf4c4035a7",
+    "url": "/React-Portfolio/static/media/directory.39822763.png"
+  },
+  {
+    "revision": "ca9718c2edf4abe77ea3ea8f9baaad32",
+    "url": "/React-Portfolio/static/media/fitness.ca9718c2.png"
+  },
+  {
+    "revision": "e33e2cc8b51115c289d4367c61a3aa91",
+    "url": "/React-Portfolio/static/media/note.e33e2cc8.png"
+  },
+  {
+    "revision": "8b260f739eb6b7bc71fbf4813b8486aa",
+    "url": "/React-Portfolio/static/media/password.8b260f73.png"
+  },
+  {
+    "revision": "47418a275cd4165792804d52913e1846",
+    "url": "/React-Portfolio/static/media/tracker.47418a27.png"
+  }
+]);
