6b71e502008a1df144238e9d731c9d7ddf477481
Don Tran <mr.dontran@gmail.com>
2020-11-02 17:18:14 -0500

    update to use comonents

--- src/App.js
+++ src/App.js
@@ -5,29 +5,38 @@ import Main from "./pages/Main"
 import Contact from "./pages/Contact"
 import Projects from "./pages/Projects"
 import About from "./pages/About"
-
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import AboutMe from "./components/AboutMe"
+import MyProjects from "./components/MyProjects"
+import ContactMe from "./components/ContactMe"
 
 
 function App() {
 
   return (
-    <HashRouter>
-    <Router>
-      <Switch>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Header />
+            <AboutMe />
+            <Footer />
+          </Route>
+          <Route exact path='/contact'>
+            <Header />
+            <ContactMe />
+            <Footer />
+          </Route>
+          <Route exact path='/projects'>
+            <Header />
+            <MyProjects />
+            <Footer />
+          </Route>
 
-        <Route exact path={'/contact'} component={Contact}>
-
-        </Route>
-        <Route exact path={'/projects'} component={Projects}>
-
-        </Route>
-        <Route exact path={'*'} component={About}>
-
-        </Route>
-
-      </Switch>
-    </Router>
-    </HashRouter>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
